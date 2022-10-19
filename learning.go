cbc79a71fc835d26bb5c022e49af8b34dcdcbf47
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-10-19 11:20:25 +0300

    Update learning.go

--- learning.go
+++ learning.go
@@ -5,11 +5,29 @@ import "fmt"
 func main() {
   fmt.Println("Hello, Go!")
 }
-
+// TASK 1
 package main
 
 import "fmt"
 
 func main() {
     fmt.Println("I like Go!")
+}
+// TASK 2
+
+package main
+
+import "fmt"
+ 
+func main() {
+  var name string
+  var age int
+  fmt.Println("Введите имя")
+  fmt.Scan(&name)
+  fmt.Println("Введите возраст")
+  fmt.Scan(&age)
+
+  fmt.Println(name, age)
+     
+
 }
